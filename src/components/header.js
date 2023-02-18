c277b69817c1f04b365d99bdb4833113d278ea0d
hrithikdass <hrithikdas@gmail.com>
2023-02-18 15:48:26 +0530

    First commit

---
+++ src/components/header.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default function Header() {
+  return (
+    <header className="header">
+      <img
+        src={require("../images/Troll-Face.png")}
+        alt="Error"
+        className="header--image"
+      />
+      <h2 className="header--title">Meme Generator</h2>
+      <h4 className="header--project">Additional Info</h4>
+    </header>
+  );
+}
---
+++ src/components/meme.js
@@ -0,0 +1,33 @@
+import React, { useState } from "react";
+import memesData from "../memesData.js";
+
+export default function Meme() {
+  const [meme, setMeme] = React.useState({
+    topText: "",
+    bottomText: "",
+    randomImage: "http://i.imgflip.com/1bij.jpg",
+  });
+  const [allMemeImages, setAllMemeImages] = React.useState(memesData);
+
+  function getMemeImage() {
+    const memesArray = allMemeImages.data.memes;
+    const randomNumber = Math.floor(Math.random() * memesArray.length);
+    const url = memesArray[randomNumber].url;
+    setMeme((prevMeme) => ({
+      ...prevMeme,
+      randomImage: url,
+    }));
+  }
+  return (
+    <main>
+      <div className="form">
+        <input type="text" placeholder="Top text" className="form--input" />
+        <input type="text" placeholder="Bottom text" className="form--input" />
+        <button onClick={getMemeImage} className="form--button">
+          Get a new meme image 🖼
+        </button>
+        <img src={meme.randomImage} alt="Error" className="meme--image" />
+      </div>
+    </main>
+  );
+}
