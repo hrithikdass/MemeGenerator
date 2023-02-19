7ccf31e64cc2fa6003283c77a8db565889cb2e77
hrithikdass <hrithikdas@gmail.com>
2023-02-19 12:35:33 +0530

    using meme api to get meme images

--- src/App.js
+++ src/App.js
@@ -1,10 +1,8 @@
 import Header from "./components/header";
 import "./App.css";
 import Meme from "./components/meme";
-import data from "./memesData.js";
 
 function App() {
-  function random() {}
   return (
     <div className="App">
       <Header />
